2eec3df247571fc157cb9c727a35628367a30fd1
zcq <zcq960921@163.com>
2019-04-20 23:52:40 +0800

    集成mybatis

---
+++ miaosha/src/main/java/com/zcq/controller/SampleController.java
@@ -0,0 +1,58 @@
+package com.zcq.controller;
+
+
+import com.zcq.domain.User;
+import com.zcq.result.CodeMsg;
+import com.zcq.result.Result;
+import com.zcq.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/demo")
+public class SampleController {
+
+	@Autowired
+    UserService userService;
+	
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public Result<String> home() {
+        return Result.success("Hello，world");
+    }
+    
+    @RequestMapping("/error")
+    @ResponseBody
+    public Result<String> error() {
+        return Result.error(CodeMsg.SESSION_ERROR);
+    }
+    
+    @RequestMapping("/hello/themaleaf")
+    public String themaleaf(Model model) {
+        model.addAttribute("name", "Joshua");
+        return "hello";
+    }
+    
+    @RequestMapping("/db/get")
+    @ResponseBody
+    public Result<User> dbGet() {
+    	User user = userService.getById(1);
+        return Result.success(user);
+    }
+    
+    
+    @RequestMapping("/db/tx")
+    @ResponseBody
+    public Result<Boolean> dbTx() {
+    	userService.tx();
+        return Result.success(true);
+    }
+    
+
+    
+    
+}
